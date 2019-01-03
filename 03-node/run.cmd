440fe4f6c5857d48a6faa71c3a6c5be475ce55e7
Sai Gudigundla <saip106@msn.com>
2019-01-02 22:14:22 -0500

    added demo for node server

--- 03-node/run.cmd
+++ 03-node/run.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-@ECHO Starting node server in a container
+@ECHO Starting node in a container
 
 docker run -it --rm --name nodejs node:10.14 node --version
--- 05-node-server/run.cmd
+++ 05-node-server/run.cmd
@@ -2,4 +2,4 @@
 
 @ECHO Starting node server in a container
 
-docker run -it --rm --name nodejs -d -v %cd%:/src -w /src node:10.14 node app.js
+docker run -it --rm --name nodejs -d -v %cd%:/src -w /src -p 8080:3000 node:10.14 node app.js
